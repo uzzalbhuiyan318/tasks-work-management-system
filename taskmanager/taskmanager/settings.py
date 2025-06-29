b94ce318f8af2fff5790d76de882931cee5776ca
uzzalbhuiyan318 <21303179@iubat.edu>
2025-06-29 15:26:39 +0600

    updated few features

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -74,12 +74,11 @@ WSGI_APPLICATION = "taskmanager.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
